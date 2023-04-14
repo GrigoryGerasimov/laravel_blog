3997ca9c97ad5ac5d09bcdae4763f5eab313f16a
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-04-15 01:38:48 +0200

    postcontroller splitting into separate monomethod-controllers done

---
+++ app/Http/Controllers/Post/DeleteController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\View;
+
+class DeleteController extends Controller
+{
+    public function __invoke(Post $post): Application|View
+    {
+        $categoriesList = Category::all();
+        $tagsList = Tag::all();
+
+        return view('post.delete', compact('post', 'categoriesList', 'tagsList'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@ Route::get('/', 'App\Http\Controllers\MainController@index')->name('main');
 Route::get('/about', 'App\Http\Controllers\AboutController@index')->name('about');
 Route::get('/contacts', 'App\Http\Controllers\ContactController@index')->name('contacts');
 
-Route::get('/posts', 'App\Http\Controllers\PostController@index')->name('post.index');
-Route::get('/post/create', 'App\Http\Controllers\PostController@create')->name('post.create');
-Route::post('/posts', 'App\Http\Controllers\PostController@store')->name('post.store');
+Route::group(['namespace' => 'App\Http\Controllers\Post'], function() {
+    Route::get('/posts', 'IndexController')->name('post.index');
+    Route::get('/post/create', 'CreateController')->name('post.create');
+    Route::post('/posts', 'StoreController')->name('post.store');
+    Route::get('/post/{post}', 'ShowController')->name('post.show');
+    Route::get('/post/{post}/edit', 'EditController')->name('post.edit');
+    Route::patch('/post/{post}', 'UpdateController')->name('post.update');
+    Route::get('/post/{post}/delete', 'DeleteController')->name('post.delete');
+    Route::delete('/post/{post}', 'DestroyController')->name('post.destroy');
+    Route::get('/post/{post}/restore', 'RestoreController');
+});
 
-Route::get('/post/{post}', 'App\Http\Controllers\PostController@show')->name('post.show');
-
-Route::get('/post/{post}/edit', 'App\Http\Controllers\PostController@edit')->name('post.edit');
-Route::patch('/post/{post}', 'App\Http\Controllers\PostController@update')->name('post.update');
-
-Route::get('/post/{post}/delete', 'App\Http\Controllers\PostController@delete')->name('post.delete');
-Route::delete('/post/{post}', 'App\Http\Controllers\PostController@destroy')->name('post.destroy');
-Route::get('/post/{post}/restore', 'App\Http\Controllers\PostController@restore');
