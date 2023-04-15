8560cd13663d784b18a4fc5e78ac5f6c7a8261eb
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-04-15 17:40:30 +0200

    postservice added

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -5,25 +5,17 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\{RedirectResponse};
 use Illuminate\Routing\Redirector;
 use App\Http\Requests\Post\StoreRequest;
+use App\Http\Services\PostService;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request): Application|RedirectResponse|Redirector
     {
-        $requestData = $request->validated();
-
-        $tagsData = $request['tags'];
-        unset($request['tags']);
-
-        $requestData['description'] = substr($requestData['content'], 0, 200);
-
-        $createdNewPost = Post::create($requestData);
-        $createdNewPost->tags()->attach($tagsData);
+        PostService::store($request->validated());
 
         return redirect(route('post.index'), 201);
     }
