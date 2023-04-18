10f9c4b248668c6f1234c7dabe4d7d85afa9993d
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-04-18 22:01:44 +0200

    filtering implemented

---
+++ app/Http/Filters/AbstractFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+abstract class AbstractFilter implements FilterInterface
+{
+    protected $queryParams = [];
+
+    public function __construct($queryParams)
+    {
+        $this->queryParams = $queryParams;
+    }
+
+    abstract public function getCallbacks(): array;
+
+    public function apply(Builder $builder)
+    {
+        foreach($this->getCallbacks() as $cbName => $cb) {
+            if (isset($this->queryParams[$cbName])) {
+                call_user_func($cb, $builder, $this->queryParams[$cbName]);
+            }
+        }
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Http\Filters\Filterable;
 
 class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Filterable;
 
     protected $table = 'posts';
 
