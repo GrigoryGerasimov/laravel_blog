1c2439779a14c5a16d14b692e40f0e0e38a90970
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-04-25 09:19:11 +0200

    auth init

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Blog</title>
 </head>
 <body>
@@ -27,6 +27,9 @@
             </li>
             <li class='nav-item'>
                 <a class='nav-link' href={{ route('admin.post.index') }}>Admin</a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link' href={{ route('login') }}>Sign In</a>
             </li>
         </ul>
     </div>
