fe75565de061d5d6a33ac16703942046f9cd5564
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-04-27 09:34:40 +0200

    post resource added, returning json data to frontend implemented

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -6,21 +6,28 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Http\Requests\Post\FilterRequest;
+use App\Http\Resources\Post\PostResource;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\View;
-use App\Http\Requests\Post\FilterRequest;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class IndexController extends Controller
 {
-    public function __invoke(FilterRequest $request): Application|View
+    public function __invoke(FilterRequest $request): Application|View|AnonymousResourceCollection
     {
         $filterRequest = $request->validated();
 
+        $filterRequest['page'] = $request['page'] ?? 1;
+        $filterRequest['per_page'] = $request['per_page'] ?? 10;
+
         $queryParams = app()->make(\App\Http\Filters\PostFilter::class, ['queryParams' => array_filter($filterRequest)]);
 
-        $postsList = Post::filter($queryParams)->paginate(10);
+        $postsList = Post::filter($queryParams)->paginate($filterRequest['per_page'], ['*'], 'page', $filterRequest['page']);
 
-        return view('post.index', compact('postsList'));
+        return PostResource::collection($postsList);
+
+        //return view('post.index', compact('postsList'));
     }
 }
 
--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -6,13 +6,16 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Http\Resources\Post\PostResource;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\View;
 
 class ShowController extends Controller
 {
-    public function __invoke(Post $post): Application|View
+    public function __invoke(Post $post): Application|View|PostResource
     {
-        return view('post.show', compact('post'));
+        return new PostResource($post);
+
+        //return view('post.show', compact('post'));
     }
 }
